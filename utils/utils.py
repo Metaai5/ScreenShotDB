79b632429cf9b7d0500295c6e8ecdec7c12b7fdb
yeonwlee <tlstprl1806@naver.com>
2024-08-17 22:18:28 +0900

    fix: prevent errors that may occur when directory does not exist

--- utils/utils.py
+++ utils/utils.py
@@ -7,6 +7,15 @@ from services.summary import make_summary
 from utils.paddle_ocr import make_wordlist
 import pandas as pd
 
+data_path = './data'  # 데이터 디렉토리
+organized_file_path = f'{data_path}/organized_images'  # 태그 및 요약이 완료된 이미지가 위치할 디렉토리
+storage_data_file_path = f'{data_path}/result_texts.csv'  # DB대용의 파일
+
+# 필요한 모든 디렉토리 생성
+os.makedirs(data_path, exist_ok=True)
+os.makedirs(organized_file_path, exist_ok=True)
+
+
 # 이미지 저장
 def save_image(image_path, save_path):
     print(image_path)
@@ -23,16 +32,16 @@ def make_dataframe(document_data):
         'tags': document_data['tags'],
         'summary': document_data['summary']
     }
-    dataframe_path = 'data/result_texts.csv'
+    
     df = pd.DataFrame(result_texts).T.reset_index()
     df.columns = ['uuid', 'text', 'file_path', 'tags', 'summary']
     df = df[['uuid', 'file_path', 'text', 'tags', 'summary']]
-    if not os.path.exists(dataframe_path):
-        df.to_csv(dataframe_path, index=False)
+    if not os.path.exists(storage_data_file_path):
+        df.to_csv(storage_data_file_path, index=False)
     else:
-        base_df = pd.read_csv(dataframe_path)
+        base_df = pd.read_csv(storage_data_file_path)
         base_df = pd.concat([base_df, df], ignore_index=True)
-        base_df.to_csv(dataframe_path, index=False)
+        base_df.to_csv(storage_data_file_path, index=False)
             
 # Gradio - pipe 역할
 def load_image(image_paths):
@@ -40,7 +49,7 @@ def load_image(image_paths):
         allowed_formats = {'.jpg', '.jpeg', '.png'}
         image_format = Path(image_path).suffix.lower()
         uuid_str = str(uuid.uuid4()) 
-        save_path = os.path.join('data/organized_images/', f'{uuid_str}{image_format}')
+        save_path = os.path.join(organized_file_path, f'{uuid_str}{image_format}')
         if image_format in allowed_formats:
             try:
                 file_path = save_image(image_path, save_path)
